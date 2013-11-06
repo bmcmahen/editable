14f0d116c97944a908ec657e01d14b8742de40d5
bmcmahen <ben.mcmahen@gmail.com>
2013-11-06 13:20:14 -0700

    removed some old code

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 
-/**
- * dependencies
- */
-
-var History = require('history')
-  , emitter = require('emitter')
-  , events = require('events')
-  , autosave = require('auto-save')(500);
-
-/**
- * Export `Editable`.
- */
+// Dependencies
+var History = require('history');
+var Emitter = require('emitter');
+var events = require('events');
+var autosave = require('auto-save')(500);
+var position = require('selection-range');
 
 module.exports = Editable;
+
 
 /**
  * Initialize new `Editable`.
@@ -35,17 +30,18 @@ function Editable(el, stack){
  * Mixins.
  */
 
-emitter(Editable.prototype);
+Emitter(Editable.prototype);
 
 /**
- * Get editable contents.
+ * Get/set editable contents.
  *
  * @return {String}
  * @api public
  */
 
 Editable.prototype.toString =
-Editable.prototype.contents = function(){
+Editable.prototype.contents = function(str){
+  if (str) return this.el.innerHTML = str;
   return this.el.innerHTML;
 };
 
@@ -74,9 +70,11 @@ Editable.prototype.enable = function(){
   this.events.bind('keyup', 'onstatechange');
   this.events.bind('click', 'onstatechange');
   this.events.bind('focus', 'onstatechange');
+  this.events.bind('mouseup');
+  this.events.bind('touchend', 'onmouseup');
   this.events.bind('keydown');
   this.events.bind('keypress');
-  this.events.bind('paste', 'onpaste');
+  this.events.bind('paste');
   this.events.bind('cut', 'onpaste');
   this.events.bind('input', 'onchange');
   this.emit('enable');
@@ -98,32 +96,6 @@ Editable.prototype.disable = function(){
 };
 
 /**
- * Get range.
- *
- * TODO: x-browser
- *
- * @return {Range}
- * @api public
- */
-
-Editable.prototype.range = function(){
-  return document.createRange();
-};
-
-/**
- * Get selection.
- *
- * TODO: x-browser
- *
- * @return {Selection}
- * @api public
- */
-
-Editable.prototype.selection = function(){
-  return window.getSelection();
-};
-
-/**
  * Undo.
  *
  * @return {Editable}
@@ -131,24 +103,36 @@ Editable.prototype.selection = function(){
  */
 
 Editable.prototype.undo = function(){
+  
   // If we are undoing for the first time in a sequence of undos
   // then we need to record the current state, in case we want
   // to redo our undo. 
-  var buf = this.isFirstUndo
-    ? this.history.vals[this.history.vals.length - 1]
-    : this.history.prev();
-
+  
+  var first = this.history.isFirstUndo;
+  var buf = this.history.prev();
   if (!buf) return;
+  if (first) this.addToHistory({ stay : true });
 
-  if (this.isFirstUndo) {
-    this.addToHistory();
-    this.history.i -= 1;
-    this.isFirstUndo = false;
-  }
-
-  this.el.innerHTML = buf;
-  position(this.el, buf.at);
+  this.contents(buf);
+  this.restoreCursor(buf);
+  this.emit('change');
   this.emit('state');
+  return this;
+};
+
+/**
+ * Given a buf state, restore our cursor to either
+ * a selection or a particular point.
+ * @param  {String} buf 
+ * @return {Editable}    
+ * @api private 
+ */
+
+Editable.prototype.restoreCursor = function(buf){
+  if (typeof buf.start != 'undefined'){
+    if (buf.end) position(this.el, buf.start, buf.end);
+    else position(this.el, buf.start);
+  }
   return this;
 };
 
@@ -162,11 +146,34 @@ Editable.prototype.undo = function(){
 Editable.prototype.redo = function(){
   var buf = this.history.next();
   if (!buf) return this;
-  this.el.innerHTML = buf;
-  position(this.el, buf.at);
+  this.contents(buf);
+  this.restoreCursor(buf);
+  this.emit('change');
   this.emit('state');
   return this;
 };
+
+/**
+ * Add current contents of el to our history, including
+ * cursor selection/position if it exists.
+ * @return {Editable} 
+ * @api private
+ */
+
+Editable.prototype.addToHistory = function(options){
+  var buf = new String(this.toString());
+  var pos = position(this.el);
+  if (pos){
+    buf.start = pos.start;
+    if (pos.start != pos.end) {
+      buf.end = pos.end;
+    }
+  }
+  this.history.add(buf, options);
+  this.emit('add-to-history');
+  return this;
+};
+
 
 /**
  * Execute the given `cmd` with `val`.
@@ -209,15 +216,31 @@ Editable.prototype.state = function(cmd){
  */
 
 Editable.prototype.onstatechange = function(e){
+  this.emit('change');
   this.emit('state', e);
   return this;
 };
 
 /**
- * onkeydown, which is used to detect the delete key for
- * triggering autosave & history.
- * @param  {Event} e 
+ * onmouseup check for a text-selection
  * @return {Editable} 
+ * @api private
+ */
+
+Editable.prototype.onmouseup = function(){
+  var pos = position(this.el);
+  if (pos && pos.start !== pos.end) {
+    this.emit('selection', pos.start, pos.end);
+  }
+  return this;
+};
+
+/**
+ * Determine if the user has pressed the delete key,
+ * which is not triggered by onkeypress, and manually
+ * trigger onkeypress.
+ * @param  {Event} e 
+ * @return {Edtiable}   
  */
 
 Editable.prototype.onkeydown = function(e){
@@ -229,32 +252,29 @@ Editable.prototype.onkeydown = function(e){
 };
 
 /**
- * onkeypress, push our current state & our cursor
- * position to history.
- * @param  {Event} e 
- * @return {Editable} 
- * @api private
+ * onchange listener, which calls `addToHistory` at
+ * specified interval.
+ * @param {Event} e
  */
 
 Editable.prototype.onkeypress = function(e){
-  if (!this.pushedToHistory){
+  this.emit('change');
+  if (!this.pushedToHistory) {
     this.addToHistory();
-    this.emit('change');
     this.pushedToHistory = true;
   }
 };
 
+
 /**
- * Emit `change` and trigger 'save' after a 
- * set duration.
- *
- * @param {Event} e
- * @return {Editable}
- * @api private
+ * onchange trigger our autosave function, which 
+ * will callback after a set duration of inactivity.
+ * @param  {Event} e 
+ * @return {Editable}  
+ * @api private 
  */
 
 Editable.prototype.onchange = function(e){
-  this.emit('change', e);
   var self = this;
   autosave(function(){
     self.pushedToHistory = false;
@@ -263,84 +283,17 @@ Editable.prototype.onchange = function(e){
   return this;
 };
 
+
 /**
- * onpaste event handler
+ * onpaste, add changes to history.
  * @param  {Event} e 
- * @return {Editable}   
+ * @return {Editable}
  */
 
 Editable.prototype.onpaste = function(e){
   this.addToHistory();
+  this.emit('save');
   this.emit('change');
   return this;
 };
-
-/**
- * Update our history with the current contents, plus
- * our cursor position.
- * @return {Editable}
- */
-
-Editable.prototype.addToHistory = funciton(){
-  var buf = new String(this.toString());
-  buf.at = position(this.el);
-  this.history.add(buf);
-  return this;
-};
-
-/**
- * Set / get caret position with `el`.
- *
- * @param {Element} el
- * @param {Number} at
- * @return {Number}
- * @api private
- */
-
-function position(el, at){
-  if (1 == arguments.length) {
-    var range = window.getSelection().getRangeAt(0);
-    var clone = range.cloneRange();
-    clone.selectNodeContents(el);
-    clone.setEnd(range.endContainer, range.endOffset);
-    return clone.toString().length;
-  }
-
-  var length = 0
-    , abort;
-
-  visit(el, function(node){
-    if (3 != node.nodeType) return;
-    length += node.textContent.length;
-    if (length >= at) {
-      if (abort) return;
-      abort = true;
-      var sel = document.getSelection();
-      var range = document.createRange();
-      var sub = length - node.textContent.length;
-      range.setStart(node, at - sub);
-      range.setEnd(node, at - sub);
-      sel.removeAllRanges();
-      sel.addRange(range);
-      return true;
-    }
-  });
-}
-
-/**
- * Walk all text nodes of `node`.
- *
- * @param {Element|Node} node
- * @param {Function} fn
- * @api private
- */
-
-function visit(node, fn){
-  var nodes = node.childNodes;
-  for (var i = 0; i < nodes.length; ++i) {
-    if (fn(nodes[i])) break;
-    visit(nodes[i], fn);
-  }
-}
-
 
